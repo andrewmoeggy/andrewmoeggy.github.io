4f15b228c338091dd0ea1c10b5cd15cbab3207fc
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2019-10-21 20:38:29 -0400

    strobe effect

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,29 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
-  $('.about__section').waypoint(function (direction) {
+  $('.about__section').waypoint((direction) => {
     if (direction == 'down') {
       $('nav').addClass('sticky');
+      $('home-li').addClass('show');
     } else {
       $('nav').removeClass('sticky');
     }
   }, {
-      offset: '60px;'
-    })
+    offset: '60px;'
+  })
 
 
-  var waypoints = $('#handler-first').waypoint(function (direction) {
+  const waypoints = $('#handler-first').waypoint((direction) => {
     notify(this.element.id + ' hit 25% from top of window')
   }, {
-      offset: '25%'
-    })
+    offset: '25%'
+  })
 
 })
+
+function strobeEffect() {
+  document.getElementById('strobe').classList.toggle('strobe-off')
+}
+
+setInterval(() => {
+  strobeEffect()
+}, 700)
